266e54a392a44168c92e99712dfb4a5799229516
Venkatramanan <goutam3@mstudio20.tss.gatech.edu>
2015-04-11 17:32:51 -0400

    1st commit

---
+++ ShoppingWithFriends/ViewController.swift
@@ -0,0 +1,107 @@
+import UIKit
+import Parse
+import ParseUI
+
+class ViewController: UIViewController, PFLogInViewControllerDelegate, PFSignUpViewControllerDelegate {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        if (PFUser.currentUser() == nil) {
+            var logInViewController = PFLogInViewController()
+            logInViewController.fields = PFLogInFields.UsernameAndPassword | PFLogInFields.LogInButton | PFLogInFields.SignUpButton | PFLogInFields.PasswordForgotten | PFLogInFields.Facebook | PFLogInFields.Twitter
+            
+            //            var logo = UILabel()
+            //            logo.text = "RideTime"
+            //            logInViewController.logInView.logo = logo
+            
+            
+            logInViewController.delegate = self
+            
+            
+            var signUpViewController = PFSignUpViewController()
+            //            signUpViewController.signUpView.logo = logo
+            signUpViewController.delegate = self
+            
+            logInViewController.signUpController = signUpViewController
+            
+            self.presentViewController(logInViewController, animated: true, completion: nil)
+            
+            
+        }
+    }
+    func logInViewController(logInController: PFLogInViewController!, shouldBeginLoginWithUsername username: String!, password: String!) -> Bool {
+        return (!username.isEmpty || !password.isEmpty)
+    }
+    
+    func logInViewController(logInController: PFLogInViewController!, didLogInUser user: PFUser!) {
+        if (PFTwitterUtils.isLinkedWithUser(user)) {
+            
+            var twitterUsername = PFTwitterUtils.twitter().screenName
+            
+            PFUser.currentUser().username = twitterUsername
+            
+            PFUser.currentUser().saveEventually(nil)
+            
+        }
+        
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    func logInViewController(logInController: PFLogInViewController!, didFailToLogInWithError error: NSError!) {
+        println("Failed to login...")
+    }
+    func signUpViewController(signUpController: PFSignUpViewController!, didSignUpUser user: PFUser!) {
+        if (PFTwitterUtils.isLinkedWithUser(user)) {
+            
+            var twitterUsername = PFTwitterUtils.twitter().screenName
+            
+            PFUser.currentUser().username = twitterUsername
+            
+            PFUser.currentUser().saveEventually(nil)
+            
+        }
+        
+        self.dismissViewControllerAnimated(true, completion: nil)
+    }
+    func signUpViewController(signUpController: PFSignUpViewController!, didFailToSignUpWithError error: NSError!) {
+        println("Failed to sign up...")
+    }
+    func signUpViewControllerDidCancelSignUp(signUpController: PFSignUpViewController!) {
+        println("User cancelled login")
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func logoutButton(sender: AnyObject) {
+        PFUser.logOut()
+        loginSetup()
+    }
+    func loginSetup() {
+        
+        if (PFUser.currentUser() == nil) {
+            
+            var logInViewController = PFLogInViewController()
+            logInViewController.fields = PFLogInFields.UsernameAndPassword | PFLogInFields.LogInButton | PFLogInFields.SignUpButton | PFLogInFields.PasswordForgotten | PFLogInFields.Facebook | PFLogInFields.Twitter
+            logInViewController.delegate = self
+            
+            var signUpViewController = PFSignUpViewController()
+            
+            signUpViewController.delegate = self
+            
+            logInViewController.signUpController = signUpViewController
+            
+            self.presentViewController(logInViewController, animated: true, completion: nil)
+            
+            
+        }
+        
+    }
+    
+}
